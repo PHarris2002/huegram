9b3b33bbd8634ccc55990d604ed045566de86349
Priscilla Harris <89226780+PHarris2002@users.noreply.github.com>
2023-12-01 15:18:20 -0600

    Add Heart

--- src/components/Hue.tsx
+++ src/components/Hue.tsx
@@ -16,8 +16,11 @@ const Hue = (props: Props) => {
       className="flex flex-col max-h-full h-64 aspect-square rounded-3xl text-center justify-between items-center"
       style={{ backgroundColor: props.hue.color }}
     >
-      <p className="text-white text-2xl opacity-80">{props.hue.color}</p>
-
+    <div className="flex w-full px-5 py-2 justify-between items-center text-white">
+      <div></div>
+      <p className="text-2xl opacity-80">{props.hue.color}</p>
+      <i className='fa-regular fa-heart text-4xl'></i>
+    </div>  
 
       <div className="bg-black text-white flex w-full text-center justify-center p-4 rounded-b-2xl">
         <p className="text-xl">{props.hue.username}</p>
